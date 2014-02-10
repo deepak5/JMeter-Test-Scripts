37fc9d526408e7499ff5da9504511cc67b77c1bd
James Fisher <james.fisher@arqiva.com>
2014-02-10 17:52:54 +0000

    remove later requests in free script; add more assertions on first request

--- test.bash
+++ test.bash
@@ -3,3 +3,6 @@
 jmeter="java -jar ./bin/apache-jmeter-2.11/bin/ApacheJMeter.jar --nongui --propfile ./jmeter.properties"
 
 ${jmeter} --testfile ./JMX/gla_free.jmx --logfile ./log/results.xml
+
+# Old MAC address: a4:4e:31:91:2b:e8
+# ifconfig wlan0 hw ether de:ad:de:ad:be:ef
