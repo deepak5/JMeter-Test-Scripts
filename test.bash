4124a792f465b546d330c681d06733592bbc7ecf
James Fisher <james.fisher@arqiva.com>
2014-02-11 17:02:28 +0000

    clean up test bash script

--- test.bash
+++ test.bash
@@ -3,15 +3,16 @@
 jmeter="java -jar ./bin/apache-jmeter-2.11/bin/ApacheJMeter.jar --nongui --propfile ./jmeter.properties"
 logfile=./log/results.xml
 
-# Clear the log file
-echo '' > ${logfile}
+function run_test {
+  # Clear the log file
+  echo '' > ${logfile}
 
-${jmeter} \
-  --addprop ./sites/GLAAIR01.properties \
-  --jmeterproperty captive_portal.domain=staging.wifiservice.net \
-  --jmeterproperty mac_address=0A:1B:2C:3D:4E:4D \
-  --testfile ./JMX/free.jmx \
-  --logfile ${logfile}
+  ${jmeter} \
+    --addprop ./sites/${1}.properties \
+    --jmeterproperty captive_portal.domain=${2} \
+    --jmeterproperty mac_address=${3} \
+    --testfile ./JMX/${4}.jmx \
+    --logfile ${logfile}
+}
 
-# Old MAC address: a4:4e:31:91:2b:e8
-# ifconfig wlan0 hw ether de:ad:de:ad:be:ef
+run_test "GLAAIR01" "staging.wifiservice.net" "0A:1B:2C:3D:4E:4D" "free"
