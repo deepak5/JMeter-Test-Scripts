a61c8184f73063537dc69cc147c5349c8c458c45
James Fisher <james.fisher@arqiva.com>
2014-02-11 10:54:49 +0000

    clear the log file before running

--- test.bash
+++ test.bash
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 jmeter="java -jar ./bin/apache-jmeter-2.11/bin/ApacheJMeter.jar --nongui --propfile ./jmeter.properties"
+logfile=./log/results.xml
 
-${jmeter} --testfile ./JMX/gla_free.jmx --logfile ./log/results.xml
+# Clear the log file
+echo '' > ${logfile}
+
+${jmeter} --testfile ./JMX/gla_free.jmx --logfile ${logfile}
 
 # Old MAC address: a4:4e:31:91:2b:e8
 # ifconfig wlan0 hw ether de:ad:de:ad:be:ef
