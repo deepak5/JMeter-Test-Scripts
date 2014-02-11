cda8836c2b79104d64ab5d172bbed37f89ec7c90
James Fisher <james.fisher@arqiva.com>
2014-02-11 16:27:38 +0000

    move MAC address to shell, hopefully we can get it at that point

--- test.bash
+++ test.bash
@@ -6,7 +6,7 @@ logfile=./log/results.xml
 # Clear the log file
 echo '' > ${logfile}
 
-${jmeter} --testfile ./JMX/gla_free.jmx --logfile ${logfile}
+${jmeter} --jmeterproperty mac_address=0A:1B:2C:3D:4E:4F --testfile ./JMX/gla_free.jmx --logfile ${logfile}
 
 # Old MAC address: a4:4e:31:91:2b:e8
 # ifconfig wlan0 hw ether de:ad:de:ad:be:ef
