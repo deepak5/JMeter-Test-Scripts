4b8affa76b9caf1a08fb779ae58c914d56a6a042
James Fisher <james.fisher@arqiva.com>
2014-02-14 17:29:09 +0000

    test heathrow rewards

--- test.bash
+++ test.bash
@@ -5,4 +5,4 @@
 # Clear the log file
 # echo '' > ${logfile}
 
-./run_test.bash "AIRLHRPUBT1" "BA:BB:CC:DD:EE:FB" "free" #> ${logfile}
+./run_test.bash "AIRLHRPUBT1" "BA:BB:CC:DD:EE:F8" "rewards" #> ${logfile}
